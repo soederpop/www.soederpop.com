d14c093a54f5738fa753f646c8b75bc771339309
soederpop <jsoeder@codal.com>
2022-02-11 02:43:38 -0600

    fixing link

--- data/projectsData.js
+++ data/projectsData.js
@@ -3,7 +3,7 @@ const projectsData = [
     title: 'ActiveMDX',
     description: `ActiveMDX provides a way for extracting key elements of your writing as data, turning collections of written documents into a database that you can use as a CMS or to automate all the things.`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: '/blog/activemdx-introduction',
   },
 ]
 
